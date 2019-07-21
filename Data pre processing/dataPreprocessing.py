52dbdf35f9134f7e43b21b2e90258464432a3e57
Vipul Bhandare <vipul.bhandare008@gmail.com>
2019-07-21 16:57:20 +0530

    first commit - data pre processing

---
+++ Data pre processing/dataPreprocessing.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 21 16:21:19 2019
+
+@author: vipulbhandare
+"""
+
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv('Data.csv')
+
+#Handle missing elements
+#independant columns
+x = dataset.iloc[:, :-1].values
+#dependant column
+y = dataset.iloc[:, 3].values
+
+#missing elements
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+#encoding
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_x = LabelEncoder()
+x[:, 0] = labelEncoder_x.fit_transform(x[:, 0])
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
+x = oneHotEncoder.fit_transform(x).toarray()
+
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
